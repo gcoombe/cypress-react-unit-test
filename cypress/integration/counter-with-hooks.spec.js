fafc1baa718dd92c22daefb830009b9d1378476f
Graham Coombe <gcoombe@zenefits.com>
2019-11-26 09:44:21 -0800

    Adds working hooks example

--- cypress/integration/counter-with-hooks.spec.js
+++ cypress/integration/counter-with-hooks.spec.js
@@ -2,11 +2,21 @@
 /// <reference types="../../lib" />
 
 import React from 'react'
+import * as ReactDOM from 'react-dom'
 import CounterWithHooks from '../../src/counter-with-hooks.jsx'
 
 /* eslint-env mocha */
 describe('CounterWithHooks component', function () {
-  it.skip('works', function () {
+
+  beforeEach(() => {
+    // We need the support file to be using the same ReactDOM instance as the spec file for hooks to work
+    cy.on('window:load', (win) => {
+      win.ReactDOM = ReactDOM
+    })
+  })
+
+
+  it('works', function () {
     cy.mount(<CounterWithHooks initialCount={3} />)
     cy.contains('3')
   })
